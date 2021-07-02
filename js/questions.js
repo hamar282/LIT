f0eddc13a99401f84807be80bfec74c92f481a4d
Khammer Mohamed <hamar282@outlook.com>
2021-07-02 23:13:12 +0100

    Update questions.js
    New update

--- js/questions.js
+++ js/questions.js
@@ -83,7 +83,7 @@ const quiz = [
 	answer: 0
 },
 {
-	q: 'Where is Stonehenge',
+	q: 'Where is Stonehenge?',
 	options: ['Wiltshire','Dorset','Somerset','Gloucestershire'],
 	answer: 0
 },
@@ -218,7 +218,7 @@ const quiz = [
 	answer: 0
 },
 {
-	q: 'How many years Romans remained in Britain',
+	q: 'How many years Romans remained in Britain?',
 	options: ['400','100','110','440'],
 	answer: 0
 },
@@ -238,12 +238,12 @@ const quiz = [
 	answer: 0
 },
 {
-	q: 'When did Roman leave Britain',
+	q: 'When did Roman leave Britain?',
 	options: ['AD 410','AD 400','AD 300', 'AD 100'],
 	answer: 0
 },
 {
-	q: 'Which tribes invaded Britains after Romans',
+	q: 'Which tribes invaded Britains after Romans?',
 	options: ['The Jutes, The Angles and the Saxons','The Jutes','The Jutes, The Angles','The Angles and the Saxons'],
 	answer: 0
 },
@@ -304,7 +304,6 @@ const quiz = [
 	answer: 0
 }
 
-
 ,
 {
 	q: 'Where did Vikings stay and form their own communitites',
@@ -323,5 +322,660 @@ const quiz = [
 	options: ['Viking','Roman','Norman','Anglo-Saxon'],
 	answer: 0
 }
+,
+{
+	q: 'When did the Battle of Hastings happen?',
+	options: ['1066','1077', '1200', '1400'],
+	answer: 0
+}
+,
+{
+	q: 'In Scotland, under which King did the people unite due to the threat of attack by Vikings?',
+	options: ['Kenneth MacAlpin','Cnut(Canute)', 'King Harold', 'None'],
+	answer: 0
+}
+,
+{
+	q: 'Which Duke invaded from northern France and defeated King Harold?',
+	options: ['William, Duke of Normandy','Kenneth MacAlpin', 'King John', 'None'],
+	answer: 0
+}
+,
+{
+	q: 'Which King was defeated by William(Duke of Normandy) in the Battle of Hastings?',
+	options: ['King Harold','Kenneth MacAlpin', 'King John', 'None'],
+	answer: 0
+},
+{
+	q: 'Who was the Danish king to rule for a short period time?',
+	options: ['Cnut(Canute)', 'Kenneth MacAlpin', 'King Harold', 'None'],
+	answer: 0
+},
+{
+	q: 'William(Duke of Normandy) defeated King Harold in the Battle of Hastings',
+	options: ['True','False'],
+	answer: 0
+},
+{
+	q: 'In which battle, the King Harold was defeated by William(Duke of Normandy)?',
+	options: ['Battle of Hastings','Battle of Bannockburn', 'Battle of Agincourt', 'None'],
+	answer: 0
+}
+,
+{
+	q: 'William became the King of England and known as William the Conqueror?',
+	options: ['Yes', 'No'],
+	answer: 0
+}
+,
+{
+	q: 'Who was known as the King of England and The Conqueror?',
+	options: ['William(Duke of Normandy)','Kenneth MacAlpin', 'King John', 'None'],
+	answer: 0
+}
+
+,
+{
+	q: 'The Battle of Hastings was commemorated in the great piece of embriodery known as',
+	options: ['Bayeux Tapestry','Tapestry Bayeux', 'The Canterbury Tales', 'None'],
+	answer: 0
+}
+,
+{
+	q: 'The Battle of Hastings was commemorated in the great piece of embriodery known as Bayeux Tapestry?',
+	options: ['True','False'],
+	answer: 0
+}
+,
+{
+	q: 'The Battle of Backburn was commemorated in the great piece of embriodery known as Bayeux Tapestry?',
+	options: ['False', 'True'],
+	answer: 0
+}
+,
+{
+	q: 'Where is Bayeux Tapestry?',
+	options: ['France','England', 'Germany', 'Scotland'],
+	answer: 0
+}
+,
+{
+	q: 'The Bayeux Tapestry is in France',
+	options: ['True','False'],
+	answer: 0
+}
+,
+{
+	q: 'The Bayeux Tapestry is in England',
+	options: ['False','True'],
+	answer: 0
+}
+,
+{
+	q: 'Was the Norman conquest was the last successful foreign invasion of England?',
+	options: ['True','False'],
+	answer: 0
+}
+,
+{
+	q: 'Was the Norman conquest was the last successful foreign invasion of Scotland?',
+	options: ['False','True'],
+	answer: 0
+}
+
+,
+{
+	q: 'What the influence for the development of the English language?',
+	options: ['Norman French','Romans','German','Anglo-Saxon'],
+	answer: 0
+}
+,
+{
+	q: 'What was the new ruling class language?',
+	options: ['Norman French','English','German','None'],
+	answer: 0
+}
+,
+{
+	q: 'Did Welsh gradually won their territories back from Normans?',
+	options: ['True','False'],
+	answer: 0
+}
+,
+{
+	q: 'Did Normans invade Scotland?',
+	options: ['False','True'],
+	answer: 0
+}
+
+,
+{
+	q: 'What was the name of the book that was written during William period?',
+	options: ['Domesday Book','The Canterbury Tales','Magna Carta','None'],
+	answer: 0
+}
+,
+{
+	q: 'What is Domesday Book?',
+	options: ['list of all towns, villages, land and animal owners','list of all towns and villages','list of all land and animal owners','None'],
+	answer: 0
+}
+,
+{
+	q: 'The Domesday Book gives a picture of society in England about?',
+	options: ['After the Norman Conquest','Before the Norman Conquest','During the Norman Conquest','None'],
+	answer: 0
+}
+,
+{
+	q: 'Was the Middle Age or Medieval period was a time of almost constant war?',
+	options: ['True','False'],
+	answer: 0
+}
+
+,
+{
+	q: 'Which King introduced the Statute of Rhuddlan?',
+	options: ['King Edward I','King Edward II','King Henry I','None'],
+	answer: 0
+}
+,
+{
+	q: 'The Statute of Rhuddlan was introduced by the King Edward I?',
+	options: ['True','False'],
+	answer: 0
+}
+,
+{
+	q: 'The Statute of Rhuddlan annexed _________ to the Crown of England',
+	options: ['Wales','England','Scotland','None'],
+	answer: 0
+}
+,
+{
+	q: 'By the middle of ____th century, the last Welsh rebeliions has been defeated',
+	options: ['15','14','13','10'],
+	answer: 0
+}
+
+,
+{
+	q: 'In Which year the Statute of Rhuddlan was introduced?',
+	options: ['1284','1212','1200','1288'],
+	answer: 0
+}
+,
+{
+	q: 'In Scotland, which kings were less successful?',
+	options: ['English','French','Ireland','Welsh'],
+	answer: 0
+}
+,
+{
+	q: 'In 1314, the Scottish was led by Robert the Bruce and defeated the English in the Battle of _________',
+	options: ['Battle of Bannockburn','Battle of Hastings', 'Battle of Agincourt', 'None'],
+	answer: 0
+}
+,
+{
+	q: 'In 1314, who defeated the English?',
+	options: ['Robert the Bruce','King Henry I','King Edward I','None'],
+	answer: 0
+}
+
+,
+{
+	q: 'By 1200, the English ruled an area of Ireland known as ________ ',
+	options: ['Pale, around Dublin','Pale, around Belfast','Both','None'],
+	answer: 0
+}
+,
+{
+	q: 'The English kings fought a long war with __________, called the Hundred Years War',
+	options: ['France','Germany','Ireland','Welsh'],
+	answer: 0
+}
+,
+{
+	q: 'The English Kings and France fought a long war called Hundred Years War?',
+	options: ['Yes','No'],
+	answer: 0
+}
+,
+{
+	q: 'The Hundred Years War was fought between _______ and ______',
+	options: ['English and France','English and Germany','English and Poland','English and Scotland'],
+	answer: 0
+}
+
+
+,
+{
+	q: 'Which King defeated the French in the Battle of Agincourt?',
+	options: ['King Henry V','King Henry IV','King Henry VI','None'],
+	answer: 0
+}
+,
+{
+	q: 'When did the Battle of Agincourt happen?',
+	options: ['1415','1400','1200','1214'],
+	answer: 0
+}
+,
+{
+	q: 'The Normans used a system of land ownership known as Feudalism?',
+	options: ['Yes','No'],
+	answer: 0
+}
+,
+{
+	q: 'In the north of Scotland and Ireland, land was owned by members of ________',
+	options: ['Clans','Gentry','Peasants','None'],
+	answer: 0
+}
+
+,
+{
+	q: 'In the south of Scotland and Ireland, land was owned by members of clans?',
+	options: ['False','True'],
+	answer: 0
+}
+,
+{
+	q: 'Which disease was called Black death?',
+	options: ['Plague','Drought','Flood','None'],
+	answer: 0
+}
+,
+{
+	q: 'In 1348, a disease came to Britain known as _______',
+	options: ['Plague (Black Death)','Drought','Flood','None'],
+	answer: 0
+}
+,
+{
+	q: 'What was the proportion of population died in England during Black death?',
+	options: ['One third','One fourth','Half','One Tenth'],
+	answer: 0
+}
+
+,
+{
+	q: 'One third of population died in Scotland during Black death',
+	options: ['False','True'],
+	answer: 0
+}
+,
+{
+	q: 'One third of population died in England during Black death',
+	options: ['True','False'],
+	answer: 0
+}
+,
+{
+	q: 'Following the Black death, the smaller population was less need to grow __________ crops',
+	options: ['Cereals','Paddy','Both','None'],
+	answer: 0
+}
+,
+{
+	q: 'The new social classes, the owners of the large areas of land are called',
+	options: ['Gentry','Clans','Peasants','None'],
+	answer: 0
+}
+,
+{
+	q: 'The new social classes, the owners of the small areas of land are called Gentry',
+	options: ['Yes','No'],
+	answer: 0
+}
+
+,
+{
+	q: 'In which age did the Parliament began to develop into the institution?',
+	options: ['Middle Age','Modern Age','Iron Age','None'],
+	answer: 0
+}
+,
+{
+	q: 'Parliament began to develop into the _______ ',
+	options: ['Institution','Authority','Organisation','All'],
+	answer: 0
+}
+,
+{
+	q: 'Who were the King\'s advisers?',
+	options: ['Important Noblemen and Leaders of the Church','Important Noblemen','Leaders of the Church','None'],
+	answer: 0
+}
+,
+{
+	q: 'Until when the King had limited powers?',
+	options: ['1215','1200','1400','1415'],
+	answer: 0
+}
+,
+{
+	q: 'Which King was forced by his noblemen to agree number of demands?',
+	options: ['King John','King Alfred the Great','Emperor Claudius','Julius Caesar'],
+	answer: 0
+}
+,
+{
+	q: 'Who forced the King John to agree to a number of demands?',
+	options: ['Noblemen','Clans','LandLords','Peasants'],
+	answer: 0
+}
+,
+{
+	q: 'What was the Charter of rights called?',
+	options: ['Magna Carta','Magna Karta','Both','None'],
+	answer: 0
+}
+,
+{
+	q: 'The Charter of rights was called Magna Carta',
+	options: ['Yes','No'],
+	answer: 0
+}
+,
+{
+	q: '________ protected the rights of the nobility and restricted the King\'s power',
+	options: ['Magna Carta','Magna Karta','None'],
+	answer: 0
+}
+,
+{
+	q: 'What was restricted on King\'s Power in Magna Carta?',
+	options: ['Collect taxes, make or change laws','Collect taxes','make or change laws','none'],
+	answer: 0
+}
+,
+{
+	q: 'Whom should the King involve to make decisions?',
+	options: ['Noblemen','Clans','LandLords','Peasants'],
+	answer: 0
+}
+,
+{
+	q: 'The Parliament is known as Houses?',
+	options: ['Yes','No'],
+	answer: 0
+}
+,
+{
+	q: 'In England, Parliament is seperated into three parts?',
+	options: ['False','True'],
+	answer: 0
+}
+,
+{
+	q: 'In England, how many parts is the Parliament is seperated?',
+	options: ['Two','One','Three'],
+	answer: 0
+}
+,
+{
+	q: 'Is the House of Lords and House of Commons are the two parts of the England Parliament?',
+	options: ['Yes','No'],
+	answer: 0
+}
+
+,
+{
+	q: 'What are the parts of the England Parliament?',
+	options: ['House of Commons, House of Lords','House of Commons','House of Lords'],
+	answer: 0
+}
+,
+{
+	q: 'Where do the nobility, great landowners and bishops sat in England Parliament?',
+	options: ['House of Lords','House of Commons','Both','None'],
+	answer: 0
+}
+,
+{
+	q: 'Where do the Knights, small landowners, wealthy people from towns and cities elected to sit?',
+	options: ['House of Commons','House of Lords','Both','None'],
+	answer: 0
+}
+,
+{
+	q: 'The nobility, small landowners and bishops sat in House of Lords?',
+	options: ['False','True'],
+	answer: 0
+}
+,
+{
+	q: 'In Middle age, only a small part of the population was able to join the electing the members of parliament',
+	options: ['True','False'],
+	answer: 0
+}
+,
+{
+	q: 'How many parts of Parliament were developed in Scotland?',
+	options: ['Three','Two','One'],
+	answer: 0
+}
+,
+{
+	q: 'In Scotland, the houses are also known as Estates?',
+	options: ['Yes','No'],
+	answer: 0
+}
+
+,
+{
+	q: 'What are the houses in Scotland?',
+	options: ['The Lords, The Commons and the Clergy','The Lords and The Commons','None'],
+	answer: 0
+}
+,
+{
+	q: 'The Judges were dependent of the government?',
+	options: ['No','Yes'],
+	answer: 0
+}
+,
+{
+	q: 'The Judges were independent of the government?',
+	options: ['Yes','No'],
+	answer: 0
+}
+,
+{
+	q: 'In England, the judges developed ________ by a process of precedence tradition',
+	options: ['Common Law','Codified','Both','None'],
+	answer: 0
+}
+,
+{
+	q: 'In England, Common law means',
+	options: ['Following the previous decisions','Codified','Both','None'],
+	answer: 0
+}
+,
+{
+	q: 'In England, Process of precedence means following the previous decisions?',
+	options: ['Yes','No'],
+	answer: 0
+}
+,
+{
+	q: 'In Scotland, the legal system is _______ ',
+	options: ['Codified','Common Law','Both','None'],
+	answer: 0
+}
+
+,
+{
+	q: 'In Scotland, is the legal system is written down?',
+	options: ['Yes','No'],
+	answer: 0
+}
+
+,
+{
+	q: 'The development of a national culture and identify was seen in the Middle ages?',
+	options: ['Yes','No'],
+	answer: 0
+}
+,
+{
+	q: 'After the Norman Conquest, what language was spoken by the King and his noblemen?',
+	options: ['Norman French','Anglo-Saxon','Both','None'],
+	answer: 0
+}
+,
+{
+	q: 'After the Norman Conquest, what language was spoken by the peasants?',
+	options: ['Anglo-Saxon','Norman French','Both','None'],
+	answer: 0
+}
+,
+{
+	q: 'After the Norman Conquest, the King and his noblemen had spoken Norman French?',
+	options: ['True','False'],
+	answer: 0
+}
+
+,
+{
+	q: 'After the Norman Conquest, the King and his noblemen had spoken Anglo-Saxon?',
+	options: ['False','True'],
+	answer: 0
+}
+
+,
+{
+	q: 'After the Norman Conquest, the peasants had spoken Norman French?',
+	options: ['False','True'],
+	answer: 0
+}
+,
+{
+	q: 'After the Norman Conquest, the peasants had spoken Anglo-Saxon?',
+	options: ['True','False'],
+	answer: 0
+}
+,
+{
+	q: 'The Norman French and Anglo-Saxon combined into one English language?',
+	options: ['True','False'],
+	answer: 0
+}
+,
+{
+	q: 'Example of Norman French word',
+	options: ['Park', 'Apple', 'Cow', 'Summer'],
+	answer: 0
+}
+
+,
+{
+	q: 'Example of Norman French word',
+	options: ['Beauty', 'Apple', 'Cow', 'Summer'],
+	answer: 0
+}
+,
+{
+	q: 'Example of Anglo-Saxon word',
+	options: ['Apple','Park', 'Beauty', 'All'],
+	answer: 0
+}
+,
+{
+	q: 'Example of Anglo-Saxon word',
+	options: ['All','Apple','Cow','Beauty'],
+	answer: 0
+}
+
+,
+{
+	q: 'Which language "Demand" is from?',
+	options: ['French', 'Anglo-Saxon', 'All', 'None'],
+	answer: 0
+}
+
+,
+{
+	q: 'Which language "Ask" is from?',
+	options: ['Anglo-Saxon','French', 'All', 'None'],
+	answer: 0
+}
+,
+{
+	q: 'In England, from when the official documents were being written in English?',
+	options: ['1400','1200', '1415', '1215'],
+	answer: 0
+}
+,
+{
+	q: 'In England, from when English had become the preferred language of royal court and Parliament?',
+	options: ['1400','1200', '1415', '1215'],
+	answer: 0
+}
+,
+{
+	q: 'In England, since 1400 what was the preferred language of royal court and Parliament?',
+	options: ['English', 'French', 'Anglo-Saxon', 'All'],
+	answer: 0
+}
+
+,
+{
+	q: 'Who wrote the series of poems in English about a group of people going to Canterbury on a pilgrimage?',
+	options: ['Geoffrey Chaucer','Kenneth MacAlpin', 'King Harold', 'None'],
+	answer: 0
+}
+,
+{
+	q: 'Geoffrey Chaucer wrote the series of poems in English about a group of people going to Canterbury on a pilgrimage ?',
+	options: ['True','False'],
+	answer: 0
+}
+,
+{
+	q: 'Geoffrey Chaucer peoms describe the travellers and the stories they told?',
+	options: ['True','False'],
+	answer: 0
+}
+
+,
+{
+	q: 'What does the Geoffrey Chaucer poems describe?',
+	options: ['about the travellers and their stories', 'about the travellers', 'about the stories', 'All'],
+	answer: 0
+}
+
+,
+{
+	q: 'What is the collection of Geoffrey Chaucer poems called?',
+	options: ['The Canterbury Tales','The Canterbury Stories', 'The Canterbury Poems', 'None'],
+	answer: 0
+}
+,
+{
+	q: 'One of the first books printed in England was?',
+	options: ['The Canterbury Tales','The Canterbury Stories', 'The Canterbury Poems', 'None'],
+	answer: 0
+}
+,
+{
+	q: 'What was the first books printed using a Printing Press in England?',
+	options: ['The Canterbury Tales','The Canterbury Stories', 'The Canterbury Poems', 'None'],
+	answer: 0
+}
+,
+{
+	q: 'Who printed the first books using a Printing Press in England?',
+	options: ['William Caxton','Geoffrey Chaucer', 'The Canterbury Poems', 'None'],
+	answer: 0
+}
+
+
+
+
+
 
 ]
